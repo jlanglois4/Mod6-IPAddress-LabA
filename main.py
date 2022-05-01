86661e34e50f48f3cc2a4ad4bb9b467db4dbb242
jlanglois4 <jlanglois4@my.wctc.edu>
2022-05-01 00:16:21 -0500

    Can now see host

--- main.py
+++ main.py
@@ -8,17 +8,44 @@ class_c_regex = r"^(19[2-9]|2[01]\d|22[0-3])(\.([1-9]?\d|[12]\d\d)){3}$"
 
 matches = []
 
+class_string_a = " Class A "
+class_string_b = " Class B "
+class_string_c = " Class C "
+
+
+def get_host(a_match, class_string):
+    if class_string == class_string_a:
+        final_match = a_match[a_match.find(".") + 1:]
+
+    if class_string == class_string_b:
+        final_match = a_match[a_match.find(".", a_match.find(".") + 1) + 1:]
+
+    if class_string == class_string_c:
+        final_match = a_match[a_match.rfind(".") + 1:]
+
+
+    return final_match
+
+
+def append_matches(match, class_string):
+    try:
+        match = match.group()
+        if match:
+            matches.append(match + class_string + get_host(match, class_string))
+    except:
+        pass
+
+
+
 for address in ip_address_list:
     match_a = re.match(class_a_regex, address)
-    match_b = re.search(class_b_regex, address)
-    match_c = re.search(class_c_regex, address)
+    match_b = re.match(class_b_regex, address)
+    match_c = re.match(class_c_regex, address)
 
-    if match_a:
-        matches.append(match_a.group() + " Class A")
-    if match_b:
-        matches.append(match_b.group() + " Class B")
-    if match_c:
-        matches.append(match_c.group() + " Class C")
+    append_matches(match_a, class_string_a)
+    append_matches(match_b, class_string_b)
+    append_matches(match_c, class_string_c)
+
 
 for item in matches:
     print(item)
